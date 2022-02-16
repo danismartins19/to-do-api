537eb166126ca2fa4014bfa410582ee4a9ed3b39
Daniel Martins <62864768+danismartins19@users.noreply.github.com>
2022-02-16 16:24:03 -0300

    Endpoints get post and put updated

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,18 +1,65 @@
-import {Request, Response} from 'express';
-import { Todo } from '../models/Todo';
+import { Request, Response } from "express";
+import { Todo } from "../models/Todo";
 
-export const all = async (req: Request, res: Response) =>{
+export const all = async (req: Request, res: Response) => {
+  let list = await Todo.findAll();
 
-}
+  if (list) {
+    res.status(200).json({
+      list,
+    });
+  } else {
+    res.status(404).json({ error: "Não há tarefas!" });
+  }
+};
 
-export const add = async (req: Request, res: Response) =>{
-    
-}
+export const add = async (req: Request, res: Response) => {
+  let { title, done } = req.body;
 
-export const update = async (req: Request, res: Response) =>{
-    
-}
+  if (!done || "false") {
+    done = false;
+  } else {
+    done = true;
+  }
 
-export const remove = async (req: Request, res: Response) =>{
-    
-}
+  let result = await Todo.create({
+    title,
+    done,
+  });
+
+  if (result) {
+    res.status(201).json({ result });
+  } else {
+    res.status(400).json({ error: "Não foi possível gravar essa tarefa!" });
+  }
+};
+
+export const update = async (req: Request, res: Response) => {
+    let  id : string = req.params.id;
+
+    let item = await Todo.findByPk(id);
+    if(item){
+        if(req.body.title){
+            item.title = req.body.title
+        }
+        if(req.body.done){
+            switch(req.body.done.toLowerCase()){
+                case 'true':
+                case '1':
+                    item.done = true;
+                    break;
+                case 'false':
+                case '0':
+                    item.done = false;
+                    break;
+            }
+        }
+
+        item.save();
+        res.status(200).json({ item });
+    } else {
+        res.json({ error : "Frase não encontrada!"});
+    }
+};
+
+export const remove = async (req: Request, res: Response) => {};
